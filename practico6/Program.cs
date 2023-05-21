419addf77ff3e9a8f5415fcc38a14fbee6292b92
SofiaaCruz <sofiaacruz187@gmail.com>
2023-05-21 18:15:24 -0300

    Ejercicio 01 c:

---
+++ practico6/Program.cs
@@ -0,0 +1,24 @@
+﻿// See https://aka.ms/new-console-template for more information
+/*Console.WriteLine("Hello, World!");
+int a;
+int b;
+a=10;
+b=a;
+Console.WriteLine("valor de a:"+a);
+Console.WriteLine("valor de b:"+b);*/
+
+Console.WriteLine("Ingrese un numero: ");
+if (int.TryParse(Console.ReadLine(), out int num)){
+
+    int invertido = 0;
+
+    while (num > 0){
+        invertido = invertido*10 + num%10;
+        num /= 10;
+    }
+
+    Console.WriteLine("El numero invertido es: " +invertido);
+}
+else{
+    Console.WriteLine("No es un numero");
+}
